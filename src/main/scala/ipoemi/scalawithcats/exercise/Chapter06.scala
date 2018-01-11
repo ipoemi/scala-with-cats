98bbf9000704319133ae7a2a7193dac72ddf4466
ipoemi <ipoemi@nate.com>
2018-01-11 16:48:39 +0900

    Chapter06.4 Complete

--- src/main/scala/ipoemi/scalawithcats/exercise/Chapter06.scala
+++ src/main/scala/ipoemi/scalawithcats/exercise/Chapter06.scala
@@ -3,7 +3,45 @@ package ipoemi.scalawithcats.exercise
 object `6.3.1.1` {
   import cats.Monad
 
-  def product[M[_]: Monad, A, B](x: M[A], y: M[B]): M[(A, B)] =
+  def product[M[_] : Monad, A, B](x: M[A], y: M[B]): M[(A, B)] =
     Monad[M].flatMap(x)(a => Monad[M].map(y)(b => (a, b)))
+}
+
+object `6.4.4` {
+  case class User(name: String, age: Int)
+
+  import cats.data.Validated
+  import cats.data.Validated._
+  import cats.syntax.either._
+  import cats.syntax.apply._
+  import cats.instances.list._
+  import cats.instances.string._
+
+  type ErrorOr[A] = Either[List[String], A]
+
+  type AllErrorsOr[A] = Validated[List[String], A]
+
+  def getValue[K, V](k: K)(m: Map[K, V]): Option[V] = m.get(k)
+
+  def parseInt(s: String): Option[Int] = Either.catchNonFatal(s.toInt).toOption
+
+  def nonBlank(s: String): Boolean = s.nonEmpty
+
+  def nonNegative(n: Int): Boolean = n >= 0
+
+  def readName(m: Map[String, String]): ErrorOr[String] =
+    getValue("name")(m)
+      .toRight(List("the name and age must be specified"))
+      .ensure(List("the name must not be blank"))(nonBlank)
+
+  def readAge(m: Map[String, String]): ErrorOr[Int] =
+    for {
+      ageStr <- getValue("age")(m).toRight(List("the name and age must be specified"))
+      age <- parseInt(ageStr).toRight(List("the age must be a valid non-negative integer"))
+    } yield age
+
+  def readUser(m: Map[String, String]): AllErrorsOr[User] =
+    (readName(m).toValidated, readAge(m).toValidated).mapN(User.apply)
+
 }
 
--- src/test/scala/ipoemi/scalawithcats/exercise/Chapter06Spec.scala
+++ src/test/scala/ipoemi/scalawithcats/exercise/Chapter06Spec.scala
@@ -2,6 +2,7 @@ package ipoemi.scalawithcats.exercise
 
 import java.math.BigInteger
 
+import cats.data.NonEmptyVector
 import org.scalatest.{Matchers, WordSpec}
 
 import scala.concurrent._
@@ -11,6 +12,7 @@ import scala.concurrent.ExecutionContext.Implicits.global
 class Chapter06Spec extends WordSpec with Matchers {
 
   "6.3.1.1 Exercise: The Product of Monads" should {
+
     import `6.3.1.1`._
     import cats._, cats.implicits._
 
@@ -32,7 +34,82 @@ class Chapter06Spec extends WordSpec with Matchers {
     "return (\"Hello\", 123)" in {
       Await.result(product(Future("Hello"), Future(123)), 1 second) shouldBe("Hello", 123)
     }
+  }
 
+  "6.4.4 Exercise: Form Validation" should {
+    import `6.4.4`._
+    import cats._, cats.data._, cats.implicits._
+
+    "return Left(List(\"the name and age must be specified\")) for the following" in {
+      readName(Map()) shouldBe Left(List("the name and age must be specified"))
+      readAge(Map()) shouldBe Left(List("the name and age must be specified"))
+    }
+
+    "return Left(List(\"the name must not be blank\")) for the following" in {
+      readName(Map("name" -> "")) shouldBe Left(List("the name must not be blank"))
+    }
+
+    "return Left(List(\"the age must be a valid non-negative integer\")) for the following" in {
+      readAge(Map("age" -> "")) shouldBe Left(List("the age must be a valid non-negative integer"))
+    }
+
+    val user1Map = Map("name" -> "user1", "age" -> "37")
+    "return Right(\"user1\") for the following readName" in {
+      readName(user1Map) shouldBe Right(user1Map("name"))
+    }
+
+    "return Right(37) for the following" in {
+      readAge(user1Map) shouldBe Right(37)
+    }
+
+    "return None for the following getValue" in {
+      getValue("name")(Map()) shouldBe None
+    }
+
+    "return Some(user1) for the following getValue" in {
+      getValue("name")(Map("name" -> "user1")) shouldBe Some("user1")
+    }
+
+    "return None for the following parseInt" in {
+      parseInt("") shouldBe None
+
+      parseInt("1a") shouldBe None
+    }
+
+    "return Some(37) for the following parseInt" in {
+      parseInt("37") shouldBe Some(37)
+    }
+
+    "return true for the following nonBlank" in {
+      nonBlank("user1") shouldBe true
+    }
+
+    "return false for the following nonBlank" in {
+      nonBlank("") shouldBe false
+    }
+
+    "return false for the following nonNegative" in {
+      nonNegative(-1) shouldBe false
+
+      nonNegative(-100) shouldBe false
+    }
+
+    "return true for the following nonNegative" in {
+      nonNegative(1) shouldBe true
+
+      nonNegative(0) shouldBe true
+    }
+
+    "return Valid(User) for the following" in {
+      var userMap = Map("name" -> "user1", "age" -> "37")
+      readUser(userMap) shouldBe Validated.valid(User("user1", 37))
+    }
+
+    "return Invalid(List(\"the name and age must be specified\", \"the name and age must be specified\")) for the following" in {
+      var userMap = Map[String, String]()
+      readUser(userMap) shouldBe
+        Validated.invalid(List("the name and age must be specified", "the name and age must be specified"))
+    }
   }
 
 }
