126f00d1436ed1e0458db143c3b60a6b1141b13c
ipoemi <ipoemi@nate.com>
2018-01-17 13:39:15 +0900

    Chapter11 Complete
    Because chapter11 exercise solution code is not passed,
    rewrite some code

--- src/main/scala/ipoemi/scalawithcats/exercise/Chapter11.scala
+++ src/main/scala/ipoemi/scalawithcats/exercise/Chapter11.scala
@@ -17,38 +17,54 @@ object `11.2.3` {
     def total: Int = counters.values.sum
 
   }
+
 }
 
 object `11.3.2` {
+
   import cats.Monoid
 
   trait BoundedSemiLattice[A] extends Monoid[A] {
     def combine(a1: A, a2: A): A
+
     def empty: A
   }
 
   object BoundedSemiLattice {
+    import cats.syntax.semigroup._
+    import cats.instances.map._
+
     def apply[A](implicit B: BoundedSemiLattice[A]) = B
 
     implicit val intBoudedSemiLattice = new BoundedSemiLattice[Int] {
       def combine(a1: Int, a2: Int): Int = a1 max a2
+
       def empty: Int = 0
     }
 
     implicit def setBoudedSemiLattice[A] = new BoundedSemiLattice[Set[A]] {
-      def combine(a1: Set[A], a2: Set[A]): Set[A] = a1 ++ a2
+      def combine(a1: Set[A], a2: Set[A]): Set[A] = a1 union a2
+
       def empty: Set[A] = Set.empty[A]
     }
+
+    implicit def mapBoudedSemiLattice[K, V](implicit b: BoundedSemiLattice[V]) =
+      new BoundedSemiLattice[Map[K, V]] {
+        def combine(a1: Map[K, V], a2: Map[K, V]): Map[K, V] =
+          a1 |+| a2
+
+        def empty: Map[K, V] = Map.empty[K, V]
+      }
   }
 
 }
 
 object `11.3.3` {
-  import cats.Monoid
-  import cats.instances.map._
-  import cats.syntax.monoid._
 
   import `11.3.2`._
+  import cats.Monoid
+  import cats.instances.map._
+  import cats.syntax.semigroup._
 
   final case class GCounter[A](counters: Map[String, A]) {
     def increment(machine: String, amount: A)(implicit M: Monoid[A]) =
@@ -61,5 +77,158 @@ object `11.3.3` {
     def total(implicit M: Monoid[A]): A = M.combineAll(counters.values)
 
   }
+
+}
+
+object `11.4` {
+
+  import `11.3.2`.BoundedSemiLattice
+  import cats.Monoid
+  import cats.instances.map._
+  import cats.syntax.semigroup._
+
+  trait GCounter[F[_, _], K, V] {
+    def increment(f: F[K, V])(k: K, v: V)(implicit m: Monoid[V]): F[K, V]
+
+    def merge(f1: F[K, V], f2: F[K, V])(implicit b: BoundedSemiLattice[V]): F[K, V]
+
+    def total(f: F[K, V])(implicit m: Monoid[V]): V
+  }
+
+  object GCounter {
+    def apply[F[_, _], K, V](implicit counter: GCounter[F, K, V]) = counter
+
+    implicit def mapGCount[K, V] = new GCounter[Map, K, V] {
+      def increment(f: Map[K, V])(k: K, v: V)(implicit m: Monoid[V]): Map[K, V] =
+        f + (k -> (v |+| f.getOrElse(k, m.empty)))
+
+      def merge(f1: Map[K, V], f2: Map[K, V])(implicit b: BoundedSemiLattice[V]): Map[K, V] =
+        f1 |+| f2
+
+      def total(f: Map[K, V])(implicit m: Monoid[V]): V =
+        m.combineAll(f.values)
+    }
+
+  }
+
+}
+
+object `11.5` {
+
+  import cats.Monoid
+  import cats.instances.list._
+  import cats.syntax.foldable._
+  import cats.syntax.semigroup._
+
+  // Monoid Dependant Definition
+  trait BoundedSemiLattice[A] {
+    def combine(a1: A, a2: A): A
+
+    def empty: A
+  }
+
+  object BoundedSemiLattice {
+    def apply[A](implicit B: BoundedSemiLattice[A]) = B
+
+  }
+
+  object BoundedSemiLatticeInstances {
+    implicit val intBoudedSemiLattice = new BoundedSemiLattice[Int] {
+      def combine(a1: Int, a2: Int): Int = a1 max a2
+
+      def empty: Int = 0
+    }
+
+    implicit def setBoudedSemiLattice[A] = new BoundedSemiLattice[Set[A]] {
+      def combine(a1: Set[A], a2: Set[A]): Set[A] = a1 union a2
+
+      def empty: Set[A] = Set.empty[A]
+    }
+
+  }
+
+  trait GCounter[F[_, _], K, V] {
+    def increment(f: F[K, V])(k: K, v: V)(implicit m: Monoid[V]): F[K, V]
+
+    def merge(f1: F[K, V], f2: F[K, V])(implicit b: BoundedSemiLattice[V]): F[K, V]
+
+    def total(f: F[K, V])(implicit m: Monoid[V], fm: Monoid[F[K, V]]): V
+  }
+
+  object GCounter {
+    def apply[F[_, _], K, V](implicit counter: GCounter[F, K, V]) = counter
+  }
+
+  object GCounterInstances {
+    import KeyValueStore._
+
+    implicit def gCounterInstance[F[_, _], K, V](implicit kvs: KeyValueStore[F]) =
+      new GCounter[F, K, V] {
+        def increment(f: F[K, V])(key: K, value: V)(implicit m: Monoid[V]): F[K, V] = {
+          val total = f.getOrElse(key, m.empty) |+| value
+          f.put(key, total)
+        }
+
+        def merge(f1: F[K, V], f2: F[K, V])(implicit b: BoundedSemiLattice[V]): F[K, V] =
+          f2.keys.map { key =>
+            (key, b.combine(f1.get(key).getOrElse(b.empty), f2.get(key).get))
+          }.foldLeft(f1) { (f, kv) =>
+            f.put(kv._1, kv._2)
+          }
+
+        def total(f: F[K, V])(implicit m: Monoid[V], km: Monoid[F[K, V]]): V =
+          f.values.combineAll
+      }
+
+  }
+
+  trait KeyValueStore[F[_, _]] {
+    def put[K, V](f: F[K, V])(k: K, v: V): F[K, V]
+
+    def get[K, V](f: F[K, V])(k: K): Option[V]
+
+    def getOrElse[K, V](f: F[K, V])(k: K, default: V): V = get(f)(k).getOrElse(default)
+
+    def values[K, V](f: F[K, V]): List[V]
+
+    def keys[K, V](f: F[K, V]): List[K]
+  }
+
+  object KeyValueStore {
+
+    def apply[F[_, _]](implicit kvs: KeyValueStore[F]) = kvs
+
+    implicit class KvsOps[F[_, _], K, V](f: F[K, V]) {
+      def put(key: K, value: V)(implicit kvs: KeyValueStore[F]): F[K, V] =
+        kvs.put(f)(key, value)
+
+      def get(key: K)(implicit kvs: KeyValueStore[F]): Option[V] =
+        kvs.get(f)(key)
+
+      def getOrElse(key: K, default: V)(implicit kvs: KeyValueStore[F]): V =
+        kvs.getOrElse(f)(key, default)
+
+      def values(implicit kvs: KeyValueStore[F]): List[V] =
+        kvs.values(f)
+
+      def keys(implicit kvs: KeyValueStore[F]): List[K] =
+        kvs.keys(f)
+    }
+
+  }
+
+  object KeyValueStoreInstances {
+    implicit val mapKeyValueStore = new KeyValueStore[Map] {
+      def put[K, V](f: Map[K, V])(k: K, v: V): Map[K, V] = f + (k -> v)
+
+      def get[K, V](f: Map[K, V])(k: K): Option[V] = f.get(k)
+
+      def values[K, V](f: Map[K, V]): List[V] = f.values.toList
+
+      def keys[K, V](f: Map[K, V]): List[K] = f.keys.toList
+    }
+
+  }
+
 }
 
--- src/test/scala/ipoemi/scalawithcats/exercise/Chapter11Spec.scala
+++ src/test/scala/ipoemi/scalawithcats/exercise/Chapter11Spec.scala
@@ -33,39 +33,40 @@ class Chapter11Spec extends WordSpec with Matchers {
   "11.3.2 Exercise: BoundedSemiLattice Instances" should {
     import `11.3.2`._
 
-    import BoundedSemiLattice._
-
     "intBoundedSemiLattice must satisfy the following" in {
-      import cats.instances.list._
+      import `11.3.2`.BoundedSemiLattice._
+      import cats.Monoid
       import cats.instances.either._
-      import cats.instances.option._
       import cats.instances.map._
+      import cats.instances.option._
       import cats.syntax.either._
       import cats.syntax.option._
       import cats.syntax.semigroup._
+      //import cats.instances.int._
 
       (1 |+| 2) shouldBe 2
       (2 |+| 1) shouldBe 2
-      (2 |+| BoundedSemiLattice[Int].empty) shouldBe 2
-      (BoundedSemiLattice[Int].empty |+| 2) shouldBe 2
+      (2 |+| Monoid[Int].empty) shouldBe 2
+      (Monoid[Int].empty |+| 2) shouldBe 2
       (1.some |+| 2.some) shouldBe 2.some
       (1.asRight[String] |+| 2.asRight[String]) shouldBe 2.asRight[String]
       (Map("key1" -> 1, "key2" -> 2) |+| Map("key1" -> 2)) shouldBe Map("key1" -> 2, "key2" -> 2)
     }
 
     "setBoundedSemiLattice must satisfy the following" in {
-      import cats.instances.list._
+      import `11.3.2`.BoundedSemiLattice._
+      import cats.Monoid
       import cats.instances.either._
-      import cats.instances.option._
       import cats.instances.map._
+      import cats.instances.option._
       import cats.syntax.either._
       import cats.syntax.option._
       import cats.syntax.semigroup._
 
       (Set(1) |+| Set(2)) shouldBe Set(1, 2)
       (Set(2) |+| Set(1)) shouldBe Set(2, 1)
-      (Set(3, 4, 5) |+| BoundedSemiLattice[Set[Int]].empty) shouldBe Set(3, 4, 5)
-      (BoundedSemiLattice[Set[Int]].empty |+| Set(3, 4, 5)) shouldBe Set(3, 4, 5)
+      (Set(3, 4, 5) |+| Monoid[Set[Int]].empty) shouldBe Set(3, 4, 5)
+      (Monoid[Set[Int]].empty |+| Set(3, 4, 5)) shouldBe Set(3, 4, 5)
       (Set(3, 4, 5).some |+| Set(4, 6, 7).some) shouldBe Set(3, 4, 5, 6, 7).some
       (Set(3, 4, 5).asRight[String] |+| Set(4, 6, 7).asRight[String]) shouldBe Set(3, 4, 5, 6, 7).asRight[String]
       (Map("key1" -> Set(3, 4, 5), "key2" -> Set(4, 5, 6)) |+| Map("key1" -> Set(5, 6, 7))) shouldBe
@@ -75,7 +76,6 @@ class Chapter11Spec extends WordSpec with Matchers {
 
   "11.3.3 Exercise: Generic GCounter" should {
     import `11.3.3`._
-
     import cats.instances.int._
 
     "GCounter must satisfy the following" in {
@@ -86,6 +86,64 @@ class Chapter11Spec extends WordSpec with Matchers {
         GCounter(Map("A" -> 2, "B" -> 2)) // int BoundedSemiLattice instance
 
       GCounter(Map("A" -> 1, "B" -> 2)).total shouldBe 3
+    }
+
+  }
+
+  "11.4 Abstracting GCounter to a Type Class" should {
+    import `11.4`._
+    //import `11.3.2`.BoundedSemiLattice._
+    import cats.instances.int._
+
+    "GCounter[Map, String, Int] must satisfy the following" in {
+      val g = GCounter[Map, String, Int]
+      g.increment(Map("A" -> 1, "B" -> 2))("A", 2) shouldBe Map("A" -> 3, "B" -> 2)
+
+      g.merge(Map("A" -> 1, "B" -> 2), Map("A" -> 2, "B" -> 2)) shouldBe
+        Map("A" -> 2, "B" -> 2) // int BoundedSemiLattice instance
+
+      g.total(Map("A" -> 1, "B" -> 2)) shouldBe 3
+    }
+
+    "GCounter[Map, String, Int] must satisfy the following2`" in {
+      val g1 = Map("a" -> 7, "b" -> 3)
+      val g2 = Map("a" -> 2, "b" -> 5)
+
+      val counter = GCounter[Map, String, Int]
+
+      counter.merge(g1, g2) shouldBe Map("a" -> 7, "b" -> 5)
+
+      counter.total(counter.merge(g1, g2)) shouldBe 12
+    }
+
+  }
+
+  "11.5 Abstracting a Key Value Store" should {
+    import `11.5`._
+    import `11.5`.BoundedSemiLatticeInstances._
+    import `11.5`.KeyValueStoreInstances._
+    import `11.5`.GCounterInstances._
+    import cats.Monoid
+    import cats.instances.map._
+    import cats.instances.int._
+
+    val counter = GCounter[Map, String, Int]
+
+    val g1 = Map("a" -> 7, "b" -> 3)
+    val g2 = Map("a" -> 2, "b" -> 5)
+
+    "GCounter[Map, String, Int].increment must satisfy the following" in {
+      counter.increment(Map("A" -> 1, "B" -> 2))("A", 2) shouldBe Map("A" -> 3, "B" -> 2)
+    }
+
+    "GCounter[Map, String, Int].merge must satisfy the following" in {
+      counter.merge(Map("A" -> 1, "B" -> 2), Map("A" -> 2, "B" -> 2)) shouldBe Map("A" -> 2, "B" -> 2)
+      counter.merge(g1, g2) shouldBe Map("a" -> 7, "b" -> 5)
+    }
+
+    "GCounter[Map, String, Int].total must satisfy the following`" in {
+      counter.total(counter.merge(g1, g2)) shouldBe 12
+      counter.total(Map("A" -> 1, "B" -> 2)) shouldBe 3
     }
 
   }
